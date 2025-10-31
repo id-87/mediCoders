593fa9daff293d3597b7bba270ea5bf7fa6aede4
[Harshit Gupta] <[hrstgpt87@gmail.com]>
2025-10-31 17:58:31 +0530

    User model database setup

--- Backend/server.js
+++ Backend/server.js
@@ -1,18 +1,13 @@
 const express=require("express")
-const bcrypt=require("bcrypt")
+const { PrismaClient } = require('@prisma/client');
+
+
 const app=express()
 
-app.post("/register",async(req,res)=>{
-    const {Name,Email,PhoneNumber,Password,Type,Details}=req.body
-    const hashp=bcrypt.hash(Password,10)
-
-
-
-})
 app.get("/",(req,res)=>{
     res.send("Healthy")
 })
 
 app.listen(3000,()=>{
-    console.log("Server is running")
+    console.log("server is running")
 })
