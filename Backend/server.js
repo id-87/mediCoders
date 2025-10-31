965e73f1412ee6d0a68152a05c6a3fd4fada60f1
[Harshit Gupta] <[hrstgpt87@gmail.com]>
2025-10-31 18:28:02 +0530

    Backend for signup

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,31 @@
 const express=require("express")
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require('./generated/prisma');
+
+const prisma=new PrismaClient()
 
 
 const app=express()
 
+app.use(express.json())
+
 app.get("/",(req,res)=>{
     res.send("Healthy")
+})
+
+
+app.post("/register",async(req,res)=>{
+    const{name,email,phone,password,type,details}=req.body
+    const resp=await prisma.user.create({
+        data:{
+            name:name,
+            email:email,
+            phone:phone,
+            password:password,
+            type:type,
+            details:details
+        }
+    })
+
 })
 
 app.listen(3000,()=>{
