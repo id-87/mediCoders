b6fd21bd07262f19d3765157fd5451b94f047584
[Harshit Gupta] <[hrstgpt87@gmail.com]>
2025-11-25 13:28:47 +0530

    ..

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,14 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Signup from './Components/Auth/Signup'
 import Login from './Components/Auth/Login'
+import Home from './Components/Home/Home'
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
       <h1>Welcome to medicoders</h1>
-      <Signup/>
-      <Login/>   
+      <Home/>   
       </>
   )
 }
--- Frontend/src/Components/Home/Home.jsx
+++ Frontend/src/Components/Home/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import Login from '../Auth/Login'
+import Signup from '../Auth/Signup'
+import DoctorPortal from '../Portals/Doctor/DoctorPortal'
+import PatientPortal from '../Portals/Patient/PatientPortal'
 const Home = () => {
   return (
     <div>
-      
+      <h1>This is the home of MediCoders</h1>
+      {/* <Login/> */}
+      {/* <Signup/> */}
+      <DoctorPortal/>
+      <PatientPortal/>
     </div>
   )
 }
