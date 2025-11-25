449b081c23b21cacb254933ce7dbd70f2fede972
[Harshit Gupta] <[hrstgpt87@gmail.com]>
2025-11-25 13:22:40 +0530

    Portal buttons

--- Frontend/src/Components/Portals/Doctor/DoctorPortal.jsx
+++ Frontend/src/Components/Portals/Doctor/DoctorPortal.jsx
@@ -3,6 +3,12 @@ import React from 'react'
 const DoctorPortal = () => {
   return (
     <div>
+        <button>Show Appointments</button>
+        <button>Edit Appointments</button>
+        <button>Edit Availability</button>
+        
+        
+
       
     </div>
   )
--- Frontend/src/Components/Portals/Patient/PatientPortal.jsx
+++ Frontend/src/Components/Portals/Patient/PatientPortal.jsx
@@ -3,6 +3,10 @@ import React from 'react'
 const PatientPortal = () => {
   return (
     <div>
+        <button>Show Doctors</button>
+        <button>Show Appointments</button>
+        <button>Book Appointment</button>
+        
       
     </div>
   )
